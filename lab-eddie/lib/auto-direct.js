e600c8ee7c618d236e5da2801677b907d743b5e0
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-27 22:17:19 -0700

    Created readme file

--- lab-eddie/lib/auto-direct.js
+++ lab-eddie/lib/auto-direct.js
@@ -2,7 +2,9 @@
 
 const fs = require('fs')
 
+//Automatically creates a data directory and folders for each model
 const autoDataDir = module.exports = function(models) {
+
   let labDir = fs.readdirSync('.');
   console.log(Object.keys(models).length)
   if (!labDir.includes('data')) fs.mkdirSync('./data');
@@ -13,6 +15,5 @@ const autoModelDir = function(models) {
   let modelKeys = Object.keys(models);
   let dataDir = fs.readdirSync('./data');
   modelKeys.forEach(key => {
-    if(!dataDir.includes(key)) fs.mkdirSync(`./data/${key}`)
-  });
+    if(!dataDir.includes(key)) fs.mkdirSync(`./data/${key}`)});
 }
