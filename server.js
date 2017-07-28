366b4701b951e831a8329729513f2c6334e81616
sharmarke <smarky292@gmail.com>
2017-07-27 19:49:57 -0700

    everything works

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const
-http = require('http'),
-Router = require('./lib/router.js'),
-PORT = process.env.PORT || 3000;
-// const router = new Router();
+const http = require('http');
+const Router = require('./lib/router.js');
+const PORT = process.env.PORT || 3000;
+const router = new Router();
 
-const server = http.createServer();
+require('./route/car-route.js')(router)
+
+const server = http.createServer(router.route());
 
 server.listen(PORT, () => {
 console.log('server up:', PORT);
